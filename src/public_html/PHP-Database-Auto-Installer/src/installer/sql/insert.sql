3f17f622d1e3b59f3da8b73ed5c753eadfc193d3
= <gabrielramos149@gmail.com>
2019-11-08 11:57:24 -0300

    Insert SQL fix

--- src/public_html/PHP-Database-Auto-Installer/src/installer/sql/insert.sql
+++ src/public_html/PHP-Database-Auto-Installer/src/installer/sql/insert.sql
@@ -1,42 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Tempo de geração: 01-Nov-2019 às 10:15
--- Versão do servidor: 10.4.8-MariaDB
--- versão do PHP: 7.3.11
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `bdLocacao`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tbMarca`
---
-
-CREATE TABLE `tbMarca` (
-  `idMarca` int(11) NOT NULL,
-  `nomeMarca` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Extraindo dados da tabela `tbMarca`
---
-
 INSERT INTO `tbMarca` (`idMarca`, `nomeMarca`) VALUES
 (1, 'Agrale'),
 (2, 'Aston Martin'),
@@ -130,28 +91,3 @@ INSERT INTO `tbMarca` (`idMarca`, `nomeMarca`) VALUES
 (90, 'Tata'),
 (91, 'Zotye'),
 (92, 'ZX Auto');
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `tbMarca`
---
-ALTER TABLE `tbMarca`
-  ADD PRIMARY KEY (`idMarca`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `tbMarca`
---
-ALTER TABLE `tbMarca`
-  MODIFY `idMarca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
